2a25adcdf5a04cbc0322fd57a776c4857cfb79fb
AlbertoGuadron <albertoguadron92@gmail.com>
2026-01-25 21:00:26 -0600

    Actualizacion de info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import './globals.css';
 import ThemeProvider from '@/components/ThemeProvider';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Alberto Guadron | Full Stack Developer',
+  description: 'Curriculum Vitae de Alberto René Guadron Ramos',
+  icons: {
+    icon: '/favicon.ico', // aquí va tu AG
+  },
+};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
